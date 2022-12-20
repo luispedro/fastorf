7b408f2b6ef2b371df76ccfcccf9d020b9b4d50b
Luis Pedro Coelho <luis@luispedro.org>
2022-12-20 10:00:12 +0800

    ENH Genetic code (code 11)

---
+++ fastorf/genetic_code.py
@@ -0,0 +1,37 @@
+def is_start(codon):
+    return codon in ['ATG', 'GTG', 'TTG']
+def is_stop(codon):
+    return codon in ['TAA', 'TAG', 'TGA']
+
+def is_start_reverse_complement(codon):
+    return codon in ['CAT', 'CAC', 'CAA']
+
+def is_stop_reverse_complement(codon):
+    return codon in ['TTA', 'CTA', 'TCA']
+
+def reverse_complement(seq):
+    return seq[::-1].translate(str.maketrans('ATGC', 'TACG'))
+
+codon2aa = {
+    'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S',
+    'TAT': 'Y', 'TAC': 'Y', 'TAA': '*', 'TAG': '*',
+    'TGT': 'C', 'TGC': 'C', 'TGA': '*', 'TGG': 'W',
+    'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
+    'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'CAT': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+    'ATT': 'I', 'ATC': 'I', 'ATA': 'I', 'ATG': 'M',
+    'ACT': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'AAT': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGT': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+    'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V',
+    'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'GAT': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+
+    }
+
+def translate(seq):
+    return ''.join([codon2aa.get(seq[i:i+3], 'X') for i in range(0, len(seq), 3)])
+
---
+++ fastorf/tests/test_genetic_code.py
@@ -0,0 +1,21 @@
+import genetic_code
+from hypothesis import given, strategies as st
+
+def test_genetic_code():
+    assert genetic_code.is_start('ATG')
+    assert not genetic_code.is_start('AAG')
+    assert not genetic_code.is_stop('ATG')
+    assert  genetic_code.is_stop('TAA')
+
+    assert genetic_code.is_start_reverse_complement(reverse_complement('ATG'))
+
+
+@given(codon=st.lists(min_size=3, max_size=3, elements=st.sampled_from('ATGC')))
+def test_reverse_comp(codon):
+    print(codon)
+    codon = ''.join(codon)
+    print(codon)
+    assert genetic_code.is_start(codon) == genetic_code.is_start_reverse_complement(reverse_complement(codon))
+    assert genetic_code.is_stop(codon) == genetic_code.is_stop_reverse_complement(reverse_complement(codon))
+
+
